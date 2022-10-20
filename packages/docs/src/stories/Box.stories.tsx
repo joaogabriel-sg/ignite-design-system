bc194f5e58d1f0d5b2ef9527dec41a1594bb368c
João Gabriel <main.joaogabrielsg@gmail.com>
2022-10-20 20:26:44 -0300

    feat: create design system

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,23 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, odio.
+      </Text>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
